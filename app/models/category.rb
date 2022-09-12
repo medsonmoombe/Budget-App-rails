85a98274553fca6b51efd0bc6014958be66285f6
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-12 11:30:36 +0200

    Update the relationship of the tables

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user, class_name: 'User'
-  has_and_belongs_to_many :transactions, class_name: 'Transaction', dependent: :destroy
+  has_and_belongs_to_many :transactions
 
   # validates :name, presence: true, length: { maximum: 255 }
   # has_one_attached :icon
