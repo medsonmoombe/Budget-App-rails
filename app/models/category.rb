1fa0dec6f6833bd8b2bb8e560026456925e3482e
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-12 11:28:40 +0200

    Update the relationship of the tables

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user, class_name: 'User'
-  has_many :transactions, class_name: 'Transactions', dependent: :destroy
+  has_and_belongs_to_many :transactions, class_name: 'Transaction', dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 255 }
-  has_one_attached :icon
+  # validates :name, presence: true, length: { maximum: 255 }
+  # has_one_attached :icon
 
   def total_amount
     transactions.sum(:amount)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user, class_name: 'User'
+  has_and_belongs_to_many :categories
 
   validates :name, :amount, presence: true
 end
